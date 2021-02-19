a063559052713df6aeb147431c203e812c5e1ab3
Tomasz Kwiatkowski <tomasz.kwiatkowski@consultant.lego.com>
2021-02-19 10:02:41 +0100

    Fix settings, remove migrate from Dockerfile_dev

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies_listsettings.py.apps.MoviesListConfig'
+    'movies_list.apps.MoviesListConfig'
 ]
 
 MIDDLEWARE = [
